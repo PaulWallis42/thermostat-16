6f9b935ff7faf5aa2427a7d0cff7830567b0ee29
paulwallis42 <paulwallis42@gmail.com>
2016-05-19 11:57:03 +0100

    can update temp in UI

--- interface.js
+++ interface.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
+
   var thermostat = new Thermostat();
-  $('#temperature').text(thermostat.temp);
+  $('#temperature').text(thermostat.currentTemp());
+
+  $("#temperature-up").on('click', function(){
+    thermostat.increaseTemp();
+    $('#temperature').text(thermostat.currentTemp());
+  })
+
+  $('#temperature-down').on('click', function(){
+    thermostat.decreaseTemp();
+    $('#temperature').text(thermostat.currentTemp());
+  })
+
+
 })
